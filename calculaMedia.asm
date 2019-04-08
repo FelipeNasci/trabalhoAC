ba10387fe4055b21d51987675046c3c77f62ef52
FelipeNasci <36546304+FelipeNasci@users.noreply.github.com>
2019-04-08 19:34:14 -0300

    Get number of keyboard

--- calculaMedia.asm
+++ calculaMedia.asm
@@ -12,8 +12,12 @@ includelib \masm32\lib\masm32.lib
 .data
 
 ;-------    STRINGS   -----------
-output db " ", 0ah, 0h              ; String seguida de nova linha e fim_de_string
+output  db ? , 0ah, 0h              ; String seguida de nova linha e fim_de_string
+input   db ? , 0ah, 0h              ; String seguida de nova linha e fim_de_string
 write_count dw 0                    ; Variavel para armazenar caracteres escritos na console
+write_c dw 0
+
+vazio   db " ", 0ah, 0h
 
 aprovado    db " ** APROVADO ** ", 0ah, 0h
 reprovado   db " ** REPROVADO ** ", 0ah, 0h
@@ -21,6 +25,7 @@ reprovado   db " ** REPROVADO ** ", 0ah, 0h
 
 ;-------    HANDLES   -----------
 chaveSaida   dd 0
+chaveEntrada dd 0
 
 ;--------------------------------
 nNotas  dd 3                        ;   Quantidade de notas
@@ -39,11 +44,40 @@ start:
     call GetStdHandle
     mov chaveSaida, eax             ;   colocando o handle de saida em um endereco de memoria
 
+    push STD_INPUT_HANDLE           ;   Capturando handle de entrada
+    call GetStdHandle
+    mov chaveEntrada, eax           ;   colocando o handle de entrada em um endereco de memoria
+
     ;Zerar registradores
     xor eax, eax
     xor ebx, ebx
     xor ecx, ecx
     xor edx, edx
+
+
+    ;   Captura nota 1
+    
+    invoke ReadConsole, chaveEntrada, addr input, sizeof input, addr write_c, NULL
+
+
+    mov esi, offset input                       ; Armazenar apontador da string em esi
+    proximo:
+        mov al, [esi]                           ; Mover caracter atual para al
+        inc esi                                 ; Apontar para o proximo caracter
+        cmp al, 48                              ; Verificar se menor que ASCII 48 - FINALIZAR
+        jl terminar
+        cmp al, 58                              ; Verificar se menor que ASCII 58 - CONTINUAR
+        jl proximo
+    terminar:
+        dec esi                                 ; Apontar para caracter anterior
+        xor al, al                              ; 0 ou NULL
+        mov [esi], al                           ; Inserir NULL logo apos o termino do numero
+
+
+    invoke atodw, addr input
+    mov nota1, eax
+    ;invoke WriteConsole, chaveSaida, addr input, sizeof input, addr write_count, NULL
+
 
         FUNCAO_SOMA:
             mov eax, nota1
@@ -69,8 +103,18 @@ start:
         FIM_A_R:
 
 
-    invoke dwtoa, media, offset output
+    invoke dwtoa, media, addr output
     invoke WriteConsole, chaveSaida, addr output, sizeof output, addr write_count, NULL
     invoke ExitProcess, 0
-  
+
+;################   NAO ENTRA AQUI POR ENQUANTO ################
+
+
+
+    
+;    invoke atodw, offset numero         ;   String para numero - O conteudo vai para eax
+;    add eax, 10
+;    invoke dwtoa, eax, offset numero    ;   Numero para String, o conteudo de eax vai para o end. de memoria da string
+
+    
 end start
